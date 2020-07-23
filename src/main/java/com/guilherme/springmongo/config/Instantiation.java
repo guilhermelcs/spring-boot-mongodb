beb43759714b9b73a5110473fec6019cc7c4d850
Guilherme Lucas <guilherme.evandf@gmail.com>
2020-07-23 11:36:08 -0300

    Author DTO

--- src/main/java/com/guilherme/springmongo/config/Instantiation.java
+++ src/main/java/com/guilherme/springmongo/config/Instantiation.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.guilherme.springmongo.dto.AuthorDTO;
 import com.guilherme.springmongo.entities.Post;
 import com.guilherme.springmongo.entities.User;
 import com.guilherme.springmongo.repositories.PostRepository;
@@ -33,11 +34,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post p1 = new Post(null, sdf.parse( "21/03/2018"),  "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", maria);
-		Post p2 = new Post(null, sdf.parse( "21/03/2018"),  "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", alex);
-		Post p3 = new Post(null, sdf.parse( "21/03/2018"),  "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", bob);
-		
 		repo.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post p1 = new Post(null, sdf.parse( "21/03/2018"),  "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", new AuthorDTO( maria ));
+		Post p2 = new Post(null, sdf.parse( "21/03/2018"),  "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", new AuthorDTO( alex ));
+		Post p3 = new Post(null, sdf.parse( "21/03/2018"),  "Partiu viagem",  "Vou viajar para São Paulo. Abraços!", new AuthorDTO ( bob ));
+		
 		postRepo.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
